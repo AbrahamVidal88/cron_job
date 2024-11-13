25ec287974a09cc45340e6bd4093b46e50427512
AbrahamVidal88 <abraham.vidal.pat@gmail.com>
2024-11-13 18:51:02 -0500

    ejecute npm run sopas

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
-cd /home3/leviata5/pruebagit.leviatan-chk.com
 # Ejecutar composer install 
 composer install --no-interaction --prefer-dist --optimize-autoloader 
 # Ejecutar 
-npm install npm install
+npm install
 # ejecutar
-npm run buil
+# npm run build
